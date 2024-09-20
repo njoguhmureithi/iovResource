195eb286c8ee881c289204dbe1bf32cafc460f6d
Bakasa <baksajoshua09@gmail.com>
2024-09-20 17:21:52 +0300

    Active menu item checker

--- resources/views/layouts/admin-nav.blade.php
+++ resources/views/layouts/admin-nav.blade.php
@@ -1,25 +1,32 @@
+@php
+    $active = $active ?? '';
+@endphp
 <div id="sidebar-scrollbar">
     <nav class="iq-sidebar-menu">
        <ul id="iq-sidebar-toggle" class="iq-menu">
-        <li>
+        <li @if($active == 'dashboard') class="active" @endif>
             <a href="{{ route('admin.dashboard') }}" class="iq-waves-effect"><i class="las la-calendar iq-arrow-left"></i><span>Dashboard</span></a>
          </li>
-          <li class="active">
-             <a href="#admin" class="iq-waves-effect" data-toggle="collapse" aria-expanded="true"><span class="ripple rippleEffect"></span><i class="ri-admin-line iq-arrow-left"></i><span>Sermons</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
+          <li @if(in_array($active, ['sermons', 'sermon-categories', 'sermon-notes']))  class="active" @endif>
+             <a href="#admin" class="iq-waves-effect" data-toggle="collapse" aria-expanded="true">
+               <span class="ripple rippleEffect"></span>
+               <i class="ri-admin-line iq-arrow-left"></i>
+               <span>Sermons</span>
+               <i class="ri-arrow-right-s-line iq-arrow-right"></i>
+            </a>
              <ul id="admin" class="iq-submenu collapse show" data-parent="#iq-sidebar-toggle">
-                <li class="active"><a href="{{ route('admin.sermons.index') }}"><i class="las la-house-damage"></i>Sermon</a></li>
-                <li><a href="{{ route('admin.sermons-category.index') }}"><i class="ri-function-line"></i>Categories</a></li>
-                <li><a href="{{ route('admin.sermon-notes.index') }}"><i class="las la-user"></i>Notes</a></li>
-                
+                <li @if($active == 'sermons') class="active" @endif><a href="{{ route('admin.sermons.index') }}"><i class="las la-house-damage"></i>Sermon</a></li>
+                <li @if($active == 'sermon-categories') class="active" @endif><a href="{{ route('admin.sermons-category.index') }}"><i class="ri-function-line"></i>Categories</a></li>
+                <li @if($active == 'sermon-notes') class="active" @endif><a href="{{ route('admin.sermon-notes.index') }}"><i class="las la-user"></i>Notes</a></li>                
              </ul>
           </li>
-          <li>
+          <li @if($active == 'publications') class="active" @endif>
              <a href="{{ route('admin.publications.index') }}" class="iq-waves-effect"><i class="las la-calendar iq-arrow-left"></i><span>Publications</span></a>
           </li>
-          <li>
+          <li @if($active == 'events') class="active" @endif>
             <a href="{{ route('admin.events.index') }}" class="iq-waves-effect"><i class="las la-calendar iq-arrow-left"></i><span>Events</span></a>
          </li>
-         <li>
+         <li @if($active == 'users') class="active" @endif>
             <a href="{{ route('admin.users.index') }}" class="iq-waves-effect"><i class="las la-user-tie iq-arrow-left"></i><span>Users</span></a>
          </li>
           
